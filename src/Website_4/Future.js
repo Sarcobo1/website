3072e954615533d802d9c61aab151cdbbb234fd4
Sarcobo1 <akkn72038@gmail.com>
2024-12-26 13:12:20 +0500

    new page

---
+++ src/Website_4/Future.js
@@ -0,0 +1,25 @@
+import React from "react";
+import BuildIcon from '@mui/icons-material/Build';
+
+
+function Future() {
+    return(
+        <>
+          <div className="text_5">Some of the best features <br /> Of Our App!</div>
+          <div className="text_6"><span>Easy to Costomize</span></div>
+          <div className="text_7">Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Itaque!</div>
+          <div className="text_8"><span>Extreme Security</span></div>
+          <div className="text_9">Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Itaque!</div>
+          <div className="text_10"><span>Costumer Support</span></div>
+          <div className="text_11">Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Itaque!</div>
+          <div className="text_12"><span>Creative Design</span></div>
+          <div className="text_13">Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Itaque!</div>
+          <div className="icon2"><button><BuildIcon/></button></div>
+          <div className="icon3"><button><BuildIcon/></button></div>
+          <div className="icon4"><button><BuildIcon/></button></div>
+          <div className="icon5"><button><BuildIcon/></button></div>
+          <div className="img_2"><img src="https://preview.colorlib.com/theme/appco/assets/img/shape/best-features.png" alt="" /></div>
+        </>
+    )
+}
+export default Future
